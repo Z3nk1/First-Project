26f802ef3525e5d89a1a1eed6054c31d0812723c
LAPTOP-J98IOHQ0\wanth <Zenki>
2024-03-21 10:37:22 +0700

    no message

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 <html>
     <p>Hello</p>
+    <div class="row md-5">
+        <lable>Username</lable>
+        <input type="text" class="form-control" id="username" name="username"></input>
+    </div>
+    <div class="row md-5">
+        <button class="btn btn-success" type="submit" style="margin: 20px;">Submit</button>
+    </div>
 </html>
